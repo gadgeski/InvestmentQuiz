cf91a055cbc13f484c09b96d23cea6d303830cb4
gadgeski <xtwh.ce5bil@gmail.com>
2025-08-15 22:37:01 +0900

    Created ContentView.swift

---
+++ InvestmentQuiz/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  InvestmentQuiz
+//
+//  Created by Dev Tech on 2025/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        QuizView()        // ← 実際のクイズ画面
+    }
+}
+
+#Preview {
+    ContentView()
+}
