091ddbb2151f842a182034d0e6f6a5aba210ec68
gadgeski <xtwh.ce5bil@gmail.com>
2025-08-15 22:37:45 +0900

    Created InvestmentQuizApp.swift

---
+++ InvestmentQuiz/App/InvestmentQuizApp.swift
@@ -0,0 +1,18 @@
+//
+//  InvestmentQuizApp.swift
+//  InvestmentQuiz
+//
+//  Created by Dev Tech on 2025/08/15.
+//
+
+// InvestmentQuizApp.swift
+import SwiftUI
+
+@main
+struct InvestmentQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()   // ← 入口
+        }
+    }
+}
